82c1c61c39b63ccf940134216051675e4fd1abad
BlueHope@work <wangxungo@msn.com>
2020-01-23 16:08:39 +0800

    daily

--- helloCpp/src/设计模式.cpp
+++ helloCpp/src/设计模式.cpp
@@ -66,6 +66,8 @@ GoF 的 23 种设计模式的分类和功能
 
 #include <string>
 
+#include <list>
+
 using namespace std;
 
 //1、单例模式
@@ -1453,16 +1455,119 @@ void steTest() {
 	return;
 };
 
-//18、观察者模式-----
+//18、观察者模式
 namespace obs {
+	class Observer
+	{
+	public:
+		virtual void Update(int) = 0;
+	};
 
+	class Subject
+	{
+	public:
+		virtual void Attach(Observer *) = 0;
+		virtual void Detach(Observer *) = 0;
+		virtual void Notify() = 0;
+	};
+
+	class ConcreteObserver : public Observer
+	{
+	public:
+		ConcreteObserver(Subject *pSubject) : m_pSubject(pSubject) {}
+
+		void Update(int value)
+		{
+			cout << "ConcreteObserver get the update. New State:" << value << endl;
+		}
+
+	private:
+		Subject *m_pSubject;
+	};
+
+	class ConcreteObserver2 : public Observer
+	{
+	public:
+		ConcreteObserver2(Subject *pSubject) : m_pSubject(pSubject) {}
+
+		void Update(int value)
+		{
+			cout << "ConcreteObserver2 get the update. New State:" << value << endl;
+		}
+
+	private:
+		Subject *m_pSubject;
+	};
+
+	class ConcreteSubject : public Subject
+	{
+	public:
+		void Attach(Observer *pObserver);
+		void Detach(Observer *pObserver);
+		void Notify();
+
+		void SetState(int state)
+		{
+			m_iState = state;
+		}
+
+	private:
+		std::list<Observer *> m_ObserverList;
+		int m_iState;
+	};
+
+	void ConcreteSubject::Attach(Observer *pObserver)
+	{
+		m_ObserverList.push_back(pObserver);
+	}
+
+	void ConcreteSubject::Detach(Observer *pObserver)
+	{
+		m_ObserverList.remove(pObserver);
+	}
+
+	void ConcreteSubject::Notify()
+	{
+		std::list<Observer *>::iterator it = m_ObserverList.begin();
+		while (it != m_ObserverList.end())
+		{
+			(*it)->Update(m_iState);
+			++it;
+		}
+	}
 };
 void obsTest() {
+	// Create Subject
+	obs::ConcreteSubject *pSubject = new obs::ConcreteSubject();
+
+	// Create Observer
+	obs::Observer *pObserver = new obs::ConcreteObserver(pSubject);
+	obs::Observer *pObserver2 = new obs::ConcreteObserver2(pSubject);
+
+	// Change the state
+	pSubject->SetState(2);
+
+	// Register the observer
+	pSubject->Attach(pObserver);
+	pSubject->Attach(pObserver2);
+
+	pSubject->Notify();
+
+	// Unregister the observer
+	pSubject->Detach(pObserver);
+
+	pSubject->SetState(3);
+	pSubject->Notify();
+
+	delete pObserver;
+	delete pObserver2;
+	delete pSubject;
+
 	return;
 };
 
 
-//中介者模式
+//中介者模式-----
 namespace mdi {
 
 };
