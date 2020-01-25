a2bd91bbe17a31416fdc96cc45564010ef662b74
BlueHope@work <wangxungo@msn.com>
2020-01-25 15:56:25 +0800

    daily

--- helloCpp/src/设计模式.cpp
+++ helloCpp/src/设计模式.cpp
@@ -21,14 +21,14 @@ GoF 的 23 种设计模式的分类和功能
 类模式  工厂方法    (类）适配器    模板方法、解释器
 
 对象模式	单例	    代理          策略
-            原型   (对象）适配器      命令
-           抽象工厂       桥接       职责链
-          建造者          装饰        状态
-                         外观        观察者
-                         享元        中介者
-                         组合        迭代器
-                                    访问者
-                                    备忘录
+			原型   (对象）适配器      命令
+		   抽象工厂       桥接       职责链
+		  建造者          装饰        状态
+						 外观        观察者
+						 享元        中介者
+						 组合        迭代器
+									访问者
+									备忘录
 
 3. GoF的23种设计模式的功能
 前面说明了 GoF 的 23 种设计模式的分类，现在对各个模式的功能进行介绍。
@@ -560,7 +560,7 @@ void prxTest() {
 };
 
 //7、适配器模式
-namespace adp{
+namespace adp {
 	// Targets
 	class Target
 	{
@@ -601,7 +601,7 @@ void adpTest() {
 };
 
 //8、桥接模式
-namespace bdg{
+namespace bdg {
 	class Implementor
 	{
 	public:
@@ -633,7 +633,7 @@ namespace bdg{
 			m_pImpl->OperationImpl();
 		}
 	};
-} 
+}
 void bdgTest() {
 	//桥接模式测试
 	bdg::Implementor *pImplObj = new bdg::ConcreteImpementor();
@@ -1167,7 +1167,7 @@ void tmpTest() {
 }
 
 //14、策略模式
-namespace stg{
+namespace stg {
 	// The abstract strategy
 	class Strategy
 	{
@@ -1567,56 +1567,56 @@ void obsTest() {
 };
 
 
-//中介者模式-----
+//中介者模式
 namespace mdi {
-	#define SAFE_DELETE(p) if (p) { delete p; p = NULL; }
-	
+#define SAFE_DELETE(p) if (p) { delete p; p = NULL; }
+
 	class Mediator;
-	
+
 	class Colleague
 	{
 	public:
-		Colleague(Mediator *pMediator) : m_pMediator(pMediator){}
-	
+		Colleague(Mediator *pMediator) : m_pMediator(pMediator) {}
+
 		virtual void Send(wchar_t *message) = 0;
-	
+
 	protected:
 		Mediator *m_pMediator;
 	};
-	
+
 	class ConcreteColleague1 : public Colleague
 	{
 	public:
-		ConcreteColleague1(Mediator *pMediator) : Colleague(pMediator){}
-	
+		ConcreteColleague1(Mediator *pMediator) : Colleague(pMediator) {}
+
 		void Send(wchar_t *message);
-	
+
 		void Notify(wchar_t *message)
 		{
-			wcout<<message<<endl;
+			wcout << message << endl;
 		}
 	};
-	
+
 	class ConcreteColleague2 : public Colleague
 	{
 	public:
-		ConcreteColleague2(Mediator *pMediator) : Colleague(pMediator){}
-	
+		ConcreteColleague2(Mediator *pMediator) : Colleague(pMediator) {}
+
 		void Send(wchar_t *message);
-	
+
 		void Notify(wchar_t *message)
 		{
-			cout<<"ConcreteColleague2 is handling the message."<<endl;
-			wcout<<message<<endl;
+			cout << "ConcreteColleague2 is handling the message." << endl;
+			wcout << message << endl;
 		}
 	};
-	
+
 	class Mediator
 	{
 	public:
 		virtual void Sent(wchar_t *message, Colleague *pColleague) = 0;
 	};
-	
+
 	class ConcreteMediator : public Mediator
 	{
 	public:
@@ -1626,43 +1626,43 @@ namespace mdi {
 			ConcreteColleague1 *pConcreteColleague1 = dynamic_cast<ConcreteColleague1 *>(pColleague);
 			if (pConcreteColleague1)
 			{
-				cout<<"The message is from ConcreteColleague1. Now mediator forward it to ConcreteColleague2"<<endl;
+				cout << "The message is from ConcreteColleague1. Now mediator forward it to ConcreteColleague2" << endl;
 				if (m_pColleague2)
 				{
-						m_pColleague2->Notify(message);
+					m_pColleague2->Notify(message);
 				}
 			}
 			else
 			{
 				if (m_pColleague1)
 				{
-						m_pColleague1->Notify(message);
+					m_pColleague1->Notify(message);
 				}
 			}
 		}
-	
+
 		void SetColleague1(Colleague *pColleague)
 		{
 			m_pColleague1 = dynamic_cast<ConcreteColleague1 *>(pColleague);
 		}
-	
+
 		void SetColleague2(Colleague *pColleague)
 		{
 			m_pColleague2 = dynamic_cast<ConcreteColleague2 *>(pColleague);
 		}
-	
+
 	private:
 		// The Mediator knows all the Colleague
 		ConcreteColleague1 *m_pColleague1;
 		ConcreteColleague2 *m_pColleague2;
 	};
-	
+
 	void ConcreteColleague1::Send(wchar_t *message)
 	{
 		// The second parameter mark where the message comes from
 		m_pMediator->Sent(message, this);
 	}
-	
+
 	void ConcreteColleague2::Send(wchar_t *message)
 	{
 		m_pMediator->Sent(message, this);
@@ -1670,18 +1670,18 @@ namespace mdi {
 
 };
 void mdiTest() {
-	 // Create the mediator
-     mdi::Mediator *pMediator = new mdi::ConcreteMediator();
- 
-     mdi::Colleague *pColleague1 = new mdi::ConcreteColleague1(pMediator);
-     mdi::Colleague *pColleague2 = new mdi::ConcreteColleague2(pMediator);
- 
-     mdi::ConcreteMediator *pConcreteMediator = dynamic_cast<mdi::ConcreteMediator *>(pMediator);
-     pConcreteMediator->SetColleague1(pColleague1);
-     pConcreteMediator->SetColleague2(pColleague2);
- 
-     wchar_t message[260] = L"Where are you from?";
-     pColleague1->Send(message);
+	// Create the mediator
+	mdi::Mediator *pMediator = new mdi::ConcreteMediator();
+
+	mdi::Colleague *pColleague1 = new mdi::ConcreteColleague1(pMediator);
+	mdi::Colleague *pColleague2 = new mdi::ConcreteColleague2(pMediator);
+
+	mdi::ConcreteMediator *pConcreteMediator = dynamic_cast<mdi::ConcreteMediator *>(pMediator);
+	pConcreteMediator->SetColleague1(pColleague1);
+	pConcreteMediator->SetColleague2(pColleague2);
+
+	wchar_t message[260] = L"Where are you from?";
+	pColleague1->Send(message);
 
 	return;
 };
@@ -1689,12 +1689,262 @@ void mdiTest() {
 //迭代器模式
 namespace itr {
 
+	typedef struct tagNode
+	{
+		int value;
+		tagNode *pNext;
+	}Node;
+
+	class JTList
+	{
+	public:
+		JTList() : m_pHead(NULL), m_pTail(NULL) {};
+		JTList(const JTList &);
+		~JTList();
+		JTList &operator=(const JTList &);
+
+		long GetCount() const;
+		Node *Get(const long index) const;
+		Node *First() const;
+		Node *Last() const;
+		bool Includes(const int &) const;
+
+		void Append(const int &);
+		void Remove(Node *pNode);
+		void RemoveAll();
+
+	private:
+		Node *m_pHead;
+		Node *m_pTail;
+		long m_lCount;
+	};
+
+	class Iterator
+	{
+	public:
+		virtual void First() = 0;
+		virtual void Next() = 0;
+		virtual bool IsDone() const = 0;
+		virtual Node *CurrentItem() const = 0;
+	};
+
+	class JTListIterator : public Iterator
+	{
+	public:
+		JTListIterator(JTList *pList) : m_pJTList(pList), m_pCurrent(NULL) {}
+
+		virtual void First();
+		virtual void Next();
+		virtual bool IsDone() const;
+		virtual Node *CurrentItem() const;
+
+	private:
+		JTList *m_pJTList;
+		Node *m_pCurrent;
+	};
+
+	JTList::~JTList()
+	{
+		Node *pCurrent = m_pHead;
+		Node *pNextNode = NULL;
+		while (pCurrent)
+		{
+			pNextNode = pCurrent->pNext;
+			delete pCurrent;
+			pCurrent = pNextNode;
+		}
+	}
+
+	long JTList::GetCount()const
+	{
+		return m_lCount;
+	}
+
+	Node *JTList::Get(const long index) const
+	{
+		// The min index is 0, max index is count - 1
+		if (index > m_lCount - 1 || index < 0)
+		{
+			return NULL;
+		}
+
+		int iPosTemp = 0;
+		Node *pNodeTemp = m_pHead;
+		while (pNodeTemp)
+		{
+			if (index == iPosTemp++)
+			{
+				return pNodeTemp;
+			}
+			pNodeTemp = pNodeTemp->pNext;
+		}
+		return NULL;
+	}
+
+	Node *JTList::First() const
+	{
+		return m_pHead;
+	}
+
+	Node *JTList::Last() const
+	{
+		return m_pTail;
+	}
+
+	bool JTList::Includes(const int &value) const
+	{
+		Node *pNodeTemp = m_pHead;
+		while (pNodeTemp)
+		{
+			if (value == pNodeTemp->value)
+			{
+				return true;
+			}
+			pNodeTemp = pNodeTemp->pNext;
+		}
+		return false;
+	}
+
+	void JTList::Append(const int &value)
+	{
+		// Create the new node
+		Node *pInsertNode = new Node;
+		pInsertNode->value = value;
+		pInsertNode->pNext = NULL;
+
+		// This list is empty
+		if (m_pHead == NULL)
+		{
+			m_pHead = m_pTail = pInsertNode;
+		}
+		else
+		{
+			m_pTail->pNext = pInsertNode;
+			m_pTail = pInsertNode;
+		}
+		++m_lCount;
+	}
+
+	void JTList::Remove(Node *pNode)
+	{
+		if (pNode == NULL || m_pHead == NULL || m_pTail == NULL)
+		{
+			return;
+		}
+
+		if (pNode == m_pHead) // If the deleting node is head node
+		{
+			Node *pNewHead = m_pHead->pNext;
+			m_pHead = pNewHead;
+		}
+		else
+		{
+			// To get the deleting node's previous node
+			Node *pPreviousNode = NULL;
+			Node *pCurrentNode = m_pHead;
+			while (pCurrentNode)
+			{
+				pPreviousNode = pCurrentNode;
+				pCurrentNode = pCurrentNode->pNext;
+				if (pCurrentNode == pNode)
+				{
+					break;
+				}
+			}
+
+			// To get the deleting node's next node
+			Node *pNextNode = pNode->pNext;
+
+			// If pNextNode is NULL, it means the deleting node is the tail node, we should change the m_pTail pointer
+			if (pNextNode == NULL)
+			{
+				m_pTail = pPreviousNode;
+			}
+
+			// Relink the list
+			pPreviousNode->pNext = pNextNode;
+		}
+
+		// Delete the node
+		delete pNode;
+		pNode = NULL;
+		--m_lCount;
+	}
+
+	void JTList::RemoveAll()
+	{
+		delete this;
+	}
+
+	void JTListIterator::First()
+	{
+		m_pCurrent = m_pJTList->First();
+	}
+
+	void JTListIterator::Next()
+	{
+		m_pCurrent = m_pCurrent->pNext;
+	}
+
+	bool JTListIterator::IsDone() const
+	{
+		return m_pCurrent == m_pJTList->Last()->pNext;
+	}
+
+	Node *JTListIterator::CurrentItem() const
+	{
+		return m_pCurrent;
+	}
+
 };
 void itrTest() {
+	itr::JTList *pJTList = new itr::JTList;
+	pJTList->Append(10);
+	pJTList->Append(20);
+	pJTList->Append(30);
+	pJTList->Append(40);
+	pJTList->Append(50);
+	pJTList->Append(60);
+	pJTList->Append(70);
+	pJTList->Append(80);
+	pJTList->Append(90);
+	pJTList->Append(100);
+
+	itr::Iterator *pIterator = new itr::JTListIterator(pJTList);
+
+	// Print the list by JTListIterator
+	for (pIterator->First(); !pIterator->IsDone(); pIterator->Next())
+	{
+		cout << pIterator->CurrentItem()->value << "->";
+	}
+	cout << "NULL" << endl;
+
+	// Test for removing
+	itr::Node *pDeleteNode = NULL;
+	for (pIterator->First(); !pIterator->IsDone(); pIterator->Next())
+	{
+		pDeleteNode = pIterator->CurrentItem();
+		if (pDeleteNode->value == 100)
+		{
+			pJTList->Remove(pDeleteNode);
+			break;
+		}
+	}
+
+	// Print the list by JTListIterator
+	for (pIterator->First(); !pIterator->IsDone(); pIterator->Next())
+	{
+		cout << pIterator->CurrentItem()->value << "->";
+	}
+	cout << "NULL" << endl;
+
+	delete pIterator;
+	delete pJTList;
+
 	return;
 };
 
-//访问者模式
+//访问者模式-----
 namespace vtr {
 
 };
@@ -1719,7 +1969,7 @@ void itpTest() {
 };
 
 
-int main(int argc , char *argv [])
+int main(int argc, char *argv[])
 {
 	cout << "单例模式" << endl;
 	sngTest();
@@ -1813,5 +2063,5 @@ int main(int argc , char *argv [])
 	itpTest();
 	system("pause");
 
-    return 0;
+	return 0;
 }
