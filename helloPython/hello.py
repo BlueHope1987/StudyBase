d45c9187df136ccaaab956c37ea9060bad65caae
BlueHope@HOME <wangxungo@msn.com>
2020-12-05 23:56:21 +0800

    daily

--- helloPython/hello.py
+++ helloPython/hello.py
@@ -1,5 +1,45 @@
 #!/usr/bin/python3
-print("Hello, World!")
+
+#https://www.runoob.com/python3/python3-basic-syntax.html Python3 基础语法
+# 第一个注释
+# 第二个注释
+
+'''
+第三注释
+第四注释
+'''
+ 
+"""
+第五注释
+第六注释
+"""
+print ("Hello, Python!")
+
+#缩进
+if True:
+    total = 'aaa' + \
+            'bbb' + \
+            'ccc' #多行斜杠
+    total2 = ['item_one', 'item_two', 'item_three',
+            'item_four', 'item_five'] #括号内多行不需要斜杠
+    print ("True")
+else:
+    print ("False")
+#字符串与转义
+str='Runoob'
+print(str)                 # 输出字符串
+print(str[0:-1])           # 输出第一个到倒数第二个的所有字符
+print(str[0])              # 输出字符串第一个字符
+print(str[2:5])            # 输出从第三个开始到第五个的字符
+print(str[2:])             # 输出从第三个开始后的所有字符
+print(str[1:5:2])          # 输出从第二个开始到第五个且每隔两个的字符
+print(str * 2)             # 输出字符串两次
+print(str + '你好')         # 连接字符串
+ 
+print('------------------------------')
+ 
+print('hello\nrunoob')      # 使用反斜杠(\)+n转义特殊字符
+print(r'hello\nrunoob')     # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
 
 
 
