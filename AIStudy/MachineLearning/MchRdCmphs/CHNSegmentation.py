30f28692cdbe22dfbf2f57b78dc179bfe5c6fa9f
BlueHope@HOME <wangxungo@msn.com>
2020-12-10 23:45:31 +0800

    daily

--- AIStudy/MachineLearning/MchRdCmphs/CHNSegmentation.py
+++ AIStudy/MachineLearning/MchRdCmphs/CHNSegmentation.py
@@ -65,3 +65,41 @@ for token in doc:
 print('---------Named Entity Recognition--------')
 for ent in doc.ents:
     print(ent.text,ent.label_)
+
+#P51 语言模型 计数和拉普拉斯平滑 测试出现概率
+
+'''
+假设输入文本A已经分词并加入标识符<s>、</s>，每句话一个列表。vocab为所有单词的列表，K为拉普拉斯平滑的参数
+    例子：
+        输入：
+        A=[['<s>','今天','天气','不错','</s>'],
+           ['<s>','我们','今天','去','划船','</s>'],
+           ['<s>','我们','今天','去','开会','</s>']]
+        vocab=['<s>','</s>','今天','我们','天气','不错','去','划船','开会']
+        调用bigram(A,vocab,1)
+        输出：
+         p(<s>|<s>)=0.083
+         p(</s>|<s>)=0.083
+         p(今天|<s>)=0.167
+         ...
+'''
+def bigram(A,vocab,K):
+    cnt={word:0 for word in vocab}
+    cnt2={word:{word2:0 for word2 in vocab} for word in vocab}
+    #cnt(word)是word在文本中出现的次数，cnt2[word][word2]是word、word2在文本中出现的次数（word2在后）
+    for sent in A:
+        for i, word in enumerate(sent):
+            cnt[word]+=1
+            if i+1<len(sent):
+                cnt2[word][sent[i+1]]+=1
+        for word in cnt2:
+            for word2 in cnt2[word]:
+                #拉普拉斯平滑
+                prob=(cnt2[word][word2]+K)/(cnt[word]+K*len(vocab)+0.0)
+                print('P({0}|{1})={2}'.format(word2,word,prob))
+
+bigram([['<s>','今天','天气','不错','</s>'],
+        ['<s>','我们','今天','去','划船','</s>'],
+        ['<s>','我们','今天','去','开会','</s>']],
+       ['<s>','</s>','今天','我们','天气','不错','去','划船','开会'],
+       1) #结果与范例不一致 哪里出了问题？数字精度？
