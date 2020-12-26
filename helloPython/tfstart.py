a471f964b75431dc5493eab67de045b0d1b281f2
BlueHope@HOME <wangxungo@msn.com>
2020-12-26 23:57:13 +0800

    daily

--- helloPython/tfstart.py
+++ helloPython/tfstart.py
@@ -128,7 +128,14 @@ with tf.compat.v1.Session(graph=graph) as session:
         distance = session.run([dist], feed_dict=feed_dict)
         print("the distance between {} and {} -> {}".format(point1_, point2_, distance))
 
-
+'''
+输入数据集：训练数据集和标签、测试数据集和标签（以及验证数据集和标签）。
+测试和验证数据集可以放在tf.constant()中。而训练数据集被放在tf.placeholder()中，这样它可以在训练期间分批输入（随机梯度下降）。
+神经网络模型及其所有的层。这可以是一个简单的完全连接的神经网络，仅由一层组成，或者由5、9、16层组成的更复杂的神经网络。
+权重矩阵和偏差矢量以适当的形状进行定义和初始化。（每层一个权重矩阵和偏差矢量）
+损失值：模型可以输出分对数矢量（估计的训练标签），并通过将分对数与实际标签进行比较，计算出损失值（具有交叉熵函数的softmax）。损失值表示估计训练标签与实际训练标签的接近程度，并用于更新权重值。
+优化器：它用于将计算得到的损失值来更新反向传播算法中的权重和偏差。
+'''
 #2. Tensorflow 中的神经网络 数据加载 我们来定义一些方便载入数据和格式化数据的方法。
 def randomize(dataset, labels):
     permutation = np.random.permutation(labels.shape[0])
@@ -147,3 +154,52 @@ def flatten_tf_array(array):
     return tf.reshape(array, [shape[0], shape[1] * shape[2] * shape[3]])
 def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
+
+print("========加载MNIST和CIFAR-10数据集==========")
+#pip install python-mnist #https://github.com/sorki/python-mnist
+from mnist import MNIST
+
+mnist_folder = './helloPython/data/mnist/' #项目根目录相对路径
+mnist_image_width = 28
+mnist_image_height = 28
+mnist_image_depth = 1
+mnist_num_labels = 10
+mnist_image_size = 28 #debuging
+mndata = MNIST(mnist_folder)
+mnist_train_dataset_, mnist_train_labels_ = mndata.load_training()
+mnist_test_dataset_, mnist_test_labels_ = mndata.load_testing()
+mnist_train_dataset, mnist_train_labels = reformat_data(mnist_train_dataset_, mnist_train_labels_, mnist_image_size, mnist_image_size, mnist_image_depth)
+mnist_test_dataset, mnist_test_labels = reformat_data(mnist_test_dataset_, mnist_test_labels_, mnist_image_size, mnist_image_size, mnist_image_depth)
+print("There are {} images, each of size {}".format(len(mnist_train_dataset), len(mnist_train_dataset[0])))
+print("Meaning each image has the size of 28*28*1 = {}".format(mnist_image_size*mnist_image_size*1))
+print("The training set contains the following {} labels: {}".format(len(np.unique(mnist_train_labels_)), np.unique(mnist_train_labels_)))
+print('Training set shape', mnist_train_dataset.shape, mnist_train_labels.shape)
+print('Test set shape', mnist_test_dataset.shape, mnist_test_labels.shape)
+train_dataset_mnist, train_labels_mnist = mnist_train_dataset, mnist_train_labels
+test_dataset_mnist, test_labels_mnist = mnist_test_dataset, mnist_test_labels
+######################################################################################
+cifar10_folder = './data/cifar10/'
+train_datasets = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', ]
+test_dataset = ['test_batch']
+c10_image_height = 32
+c10_image_width = 32
+c10_image_depth = 3
+c10_num_labels = 10
+with open(cifar10_folder + test_dataset[0], 'rb') as f0:
+    c10_test_dict = pickle.load(f0, encoding='bytes')
+c10_test_dataset, c10_test_labels = c10_test_dict[b'data'], c10_test_dict[b'labels']
+test_dataset_cifar10, test_labels_cifar10 = reformat_data(c10_test_dataset, c10_test_labels, c10_image_size, c10_image_size, c10_image_depth)
+c10_train_dataset, c10_train_labels = [], []
+for train_dataset in train_datasets:
+    with open(cifar10_folder + train_dataset, 'rb') as f0:
+        c10_train_dict = pickle.load(f0, encoding='bytes')
+        c10_train_dataset_, c10_train_labels_ = c10_train_dict[b'data'], c10_train_dict[b'labels']
+        c10_train_dataset.append(c10_train_dataset_)
+        c10_train_labels += c10_train_labels_
+c10_train_dataset = np.concatenate(c10_train_dataset, axis=0)
+train_dataset_cifar10, train_labels_cifar10 = reformat_data(c10_train_dataset, c10_train_labels, c10_image_size, c10_image_size, c10_image_depth)
+del c10_train_dataset
+del c10_train_labels
+print("The training set contains the following labels: {}".format(np.unique(c10_train_dict[b'labels'])))
+print('Training set shape', train_dataset_cifar10.shape, train_labels_cifar10.shape)
+print('Test set shape', test_dataset_cifar10.shape, test_labels_cifar10.shape)
