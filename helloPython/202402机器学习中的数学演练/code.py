94a3922c3fb3f5c0be524e98fe0d224032bb8120
BlueHope@HOME <wangxungo@msn.com>
2024-04-10 23:56:02 +0800

    daily

--- helloPython/202402机器学习中的数学演练/code.py
+++ helloPython/202402机器学习中的数学演练/code.py
@@ -32,3 +32,33 @@ print("4-2-(1)求和符号")
 a=np.ones(1000) #[1 1 1 ... 1]
 b=np.arange(1,1001) #[1 2 3 ... 1000]
 print(a.dot(b))
+
+print("4-2-(2)绘制梯度的图形")
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+def f(w0,w1):
+    return w0**2+w0*w1+3
+def df_dw0(w0,w1):
+    return 2*w0+2*w1
+def df_dw1(w0,w1):
+    return 2*w0+0*w1
+
+w_range=2
+dw=0.25
+w0=np.arange(-w_range,w_range+dw,dw)
+w1=np.arange(-w_range,w_range+dw,dw)
+
+ww0, ww1=np.meshgrid(w0,w1)
+
+ff=np.zeros((len(w0),len(w1)))
+dff_dw0=np.zeros((len(w0),len(w1)))
+dff_dw1=np.zeros((len(w0),len(w1)))
+for i0 in range(len(w0)):
+    for i1 in range(len(w1)):
+        ff[i1,i0]=f(w0[i0],w1[i1])
+        dff_dw0[i1,i0]=df_dw0(w0[i0],w1[i1])
+        dff_dw1[i1,i0]=df_dw1(w0[i0],w1[i1])
+
+plt.figure(figsize=(9,4))
+plt.subplots_adjust(wspace=0.3)
