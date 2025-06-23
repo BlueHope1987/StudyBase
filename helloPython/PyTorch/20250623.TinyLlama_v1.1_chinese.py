a49b5cd5b9f180d0182275184dd482c399800e14
BlueHope@WorkVPC11 <wangxungo@msn.com>
2025-06-23 18:44:59 +0800

    daily

---
+++ helloPython/PyTorch/20250623.TinyLlama_v1.1_chinese.py
@@ -0,0 +1,33 @@
+#https://hf-mirror.com/TinyLlama/TinyLlama_v1.1_chinese
+#1.1B参数量的大语言模型
+'''
+下载：
+set GIT_LFS_SKIP_SMUDGE=1
+git clone https://hf-mirror.com/TinyLlama/TinyLlama_v1.1_chinese
+cd TinyLlama_v1.1_chinese
+git lfs pull --include="*.bin"
+'''
+
+from transformers import AutoTokenizer
+import transformers 
+import torch
+model = r"helloPython\_Datasets\TinyLlama_v1.1_chinese"
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+
+sequences = pipeline(
+    'The TinyLlama project aims to pretrain a 1.1B Llama model on 3 trillion tokens. With some proper optimization, we can achieve this within a span of "just" 90 days using 16 A100-40G GPUs 🚀🚀. The training has started on 2023-09-01.',
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    repetition_penalty=1.5,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=500,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
